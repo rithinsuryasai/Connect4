61544123b3ad0c883ed4f1123cff2041bd23c29c
sumedhvdatar <sumedh.datar@gmail.com>
2017-10-05 00:53:01 -0500

    Implemented calculating score

--- MaxConnect4Game.py
+++ MaxConnect4Game.py
@@ -23,6 +23,112 @@ class maxConnect4Game:
         random.seed()
 
     # Count the number of pieces already played
+
+    def eval_function(self,state):
+        """ Simple heuristic to evaluate board configurations
+            Heuristic is (num of 4-in-a-rows)*99999 + (num of 3-in-a-rows)*100 +
+            (num of 2-in-a-rows)*10 - (num of opponent 4-in-a-rows)*99999 - (num of opponent
+            3-in-a-rows)*100 - (num of opponent 2-in-a-rows)*10
+        """
+        if self.currentTurn == 1:
+            o_color = 2
+        elif self.currentTurn == 2:
+            o_color = 1
+
+        my_fours = self.checkForStreak(state,self.currentTurn, 4)
+        print my_fours
+        my_threes = self.checkForStreak(state, self.currentTurn, 3)
+        print my_threes
+        my_twos = self.checkForStreak(state,self.currentTurn, 2)
+        print my_twos
+        opp_fours = self.checkForStreak(state, o_color, 4)
+        print opp_fours
+        # opp_threes = self.checkForStreak(state, o_color, 3)
+        # opp_twos = self.checkForStreak(state, o_color, 2)
+        if opp_fours > 0:
+            return -100000
+        else:
+            return my_fours * 100000 + my_threes * 100 + my_twos
+
+    def checkForStreak(self, state, color, streak):
+        count = 0
+        # for each piece in the board...
+        for i in range(6):
+            for j in range(7):
+                # ...that is of the color we're looking for...
+                if state[i][j] == color:
+                    # check if a vertical streak starts at (i, j)
+                    count += self.verticalStreak(i, j, state, streak)
+
+                    # check if a horizontal four-in-a-row starts at (i, j)
+                    count += self.horizontalStreak(i, j, state, streak)
+
+                    # check if a diagonal (either way) four-in-a-row starts at (i, j)
+                    count += self.diagonalCheck(i, j, state, streak)
+        # return the sum of streaks of length 'streak'
+        return count
+
+    def verticalStreak(self, row, col, state, streak):
+        consecutiveCount = 0
+        for i in range(row, 6):
+            if state[i][col] == state[row][col]:
+                consecutiveCount += 1
+            else:
+                break
+
+        if consecutiveCount >= streak:
+            return 1
+        else:
+            return 0
+
+    def horizontalStreak(self, row, col, state, streak):
+        consecutiveCount = 0
+        for j in range(col, 7):
+            if state[row][j]== state[row][col]:
+                consecutiveCount += 1
+            else:
+                break
+
+        if consecutiveCount >= streak:
+            return 1
+        else:
+            return 0
+
+    def diagonalCheck(self, row, col, state, streak):
+
+        total = 0
+        # check for diagonals with positive slope
+        consecutiveCount = 0
+        j = col
+        for i in range(row, 6):
+            if j > 6:
+                break
+            elif state[i][j] == state[row][col]:
+                consecutiveCount += 1
+            else:
+                break
+            j += 1  # increment column when row is incremented
+
+        if consecutiveCount >= streak:
+            total += 1
+
+        # check for diagonals with negative slope
+        consecutiveCount = 0
+        j = col
+        for i in range(row, -1, -1):
+            if j > 6:
+                break
+            elif state[i][j] == state[row][col]:
+                consecutiveCount += 1
+            else:
+                break
+            j += 1  # increment column when row is incremented
+
+        if consecutiveCount >= streak:
+            total += 1
+
+        return total
+
     def checkPieceCount(self):
         self.pieceCount = sum(1 for row in self.gameBoard for piece in row if piece)
 
@@ -73,8 +179,8 @@ class maxConnect4Game:
                     #return np.argmax(score_list)
                 else:
                     #print self.gameBoard
-                    print "first tree"
-                    print self.gameBoard
+                    #print "first tree"
+                    #print self.gameBoard
                     score_list.append(self.min_value(self.gameBoard))
                     self.gameBoard = copy.deepcopy(current_state)
         for s in score_list:
@@ -94,13 +200,17 @@ class maxConnect4Game:
                 self.gameBoard = copy.copy(parent_node)
 
         if track_of_child_nodes == []:
-            return self.player1Score - self.player2Score
+            #print "This is the score for maximum"
+            #print self.player1Score
+            score = self.eval_function(self,self.gameBoard)
+            self.countScore1(self.dashBoard)
+            return self.player1Score
         else:
             max_score_list = []
             for child in track_of_child_nodes:
                 # print self.pieceCount
-                print "This is for Max Player"
-                print child
+                #print "This is for Max Player"
+                #print child
                 self.gameBoard = copy.deepcopy(child)
                 v = max(v, self.min_value(child))
                 #print "Value of v from max"
@@ -126,14 +236,14 @@ class maxConnect4Game:
                 self.gameBoard = copy.copy(parent_node)
 
         if track_of_child_nodes == []:
-            print "this is max score"
-            print self.player1Score
-            return self.player1Score - self.player2Score
+            #print "this is the final score for minimum"
+            self.countScore1(self.gameBoard)
+            return self.player1Score
         else:
             for child in track_of_child_nodes:
                 #print self.pieceCount
-                print "This is for opponent"
-                print child
+                #print "This is for opponent"
+                #print child
                 self.gameBoard = copy.deepcopy(child)
                 v = min(v,self.max_value(child))
                 #print "this is the value of v which will be sent"
@@ -156,7 +266,6 @@ class maxConnect4Game:
                 self.currentTurn = 2
             elif self.currentTurn == 2:
                 self.currentTurn = 1
-
     # Calculate the number of 4-in-a-row each player has
     def countScore(self):
         self.player1Score = 0;
@@ -357,3 +466,204 @@ class maxConnect4Game:
         if (self.gameBoard[2][6] == 2 and self.gameBoard[3][5] == 2 and
                self.gameBoard[4][4] == 2 and self.gameBoard[5][3] == 2):
             self.player2Score += 1
+
+    def countScore1(self,state):
+        self.player1Score = 0;
+        self.player2Score = 0;
+
+        # Check horizontally
+        for row in state:
+            # Check player 1
+            if row[0:4] == [1]*4:
+                self.player1Score += 1
+            if row[1:5] == [1]*4:
+                self.player1Score += 1
+            if row[2:6] == [1]*4:
+                self.player1Score += 1
+            if row[3:7] == [1]*4:
+                self.player1Score += 1
+            # Check player 2
+            if row[0:4] == [2]*4:
+                self.player2Score += 1
+            if row[1:5] == [2]*4:
+                self.player2Score += 1
+            if row[2:6] == [2]*4:
+                self.player2Score += 1
+            if row[3:7] == [2]*4:
+                self.player2Score += 1
+
+        # Check vertically
+        for j in range(7):
+            # Check player 1
+            if (self.gameBoard[0][j] == 1 and self.gameBoard[1][j] == 1 and
+                   self.gameBoard[2][j] == 1 and self.gameBoard[3][j] == 1):
+                self.player1Score += 1
+            if (self.gameBoard[1][j] == 1 and self.gameBoard[2][j] == 1 and
+                   self.gameBoard[3][j] == 1 and self.gameBoard[4][j] == 1):
+                self.player1Score += 1
+            if (self.gameBoard[2][j] == 1 and self.gameBoard[3][j] == 1 and
+                   self.gameBoard[4][j] == 1 and self.gameBoard[5][j] == 1):
+                self.player1Score += 1
+            # Check player 2
+            if (self.gameBoard[0][j] == 2 and self.gameBoard[1][j] == 2 and
+                   self.gameBoard[2][j] == 2 and self.gameBoard[3][j] == 2):
+                self.player2Score += 1
+            if (self.gameBoard[1][j] == 2 and self.gameBoard[2][j] == 2 and
+                   self.gameBoard[3][j] == 2 and self.gameBoard[4][j] == 2):
+                self.player2Score += 1
+            if (self.gameBoard[2][j] == 2 and self.gameBoard[3][j] == 2 and
+                   self.gameBoard[4][j] == 2 and self.gameBoard[5][j] == 2):
+                self.player2Score += 1
+
+        # Check diagonally
+
+        # Check player 1
+        if (self.gameBoard[2][0] == 1 and self.gameBoard[3][1] == 1 and
+               self.gameBoard[4][2] == 1 and self.gameBoard[5][3] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][0] == 1 and self.gameBoard[2][1] == 1 and
+               self.gameBoard[3][2] == 1 and self.gameBoard[4][3] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][1] == 1 and self.gameBoard[3][2] == 1 and
+               self.gameBoard[4][3] == 1 and self.gameBoard[5][4] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][0] == 1 and self.gameBoard[1][1] == 1 and
+               self.gameBoard[2][2] == 1 and self.gameBoard[3][3] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][1] == 1 and self.gameBoard[2][2] == 1 and
+               self.gameBoard[3][3] == 1 and self.gameBoard[4][4] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][2] == 1 and self.gameBoard[3][3] == 1 and
+               self.gameBoard[4][4] == 1 and self.gameBoard[5][5] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][1] == 1 and self.gameBoard[1][2] == 1 and
+               self.gameBoard[2][3] == 1 and self.gameBoard[3][4] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][2] == 1 and self.gameBoard[2][3] == 1 and
+               self.gameBoard[3][4] == 1 and self.gameBoard[4][5] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][3] == 1 and self.gameBoard[3][4] == 1 and
+               self.gameBoard[4][5] == 1 and self.gameBoard[5][6] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][2] == 1 and self.gameBoard[1][3] == 1 and
+               self.gameBoard[2][4] == 1 and self.gameBoard[3][5] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][3] == 1 and self.gameBoard[2][4] == 1 and
+               self.gameBoard[3][5] == 1 and self.gameBoard[4][6] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][3] == 1 and self.gameBoard[1][4] == 1 and
+               self.gameBoard[2][5] == 1 and self.gameBoard[3][6] == 1):
+            self.player1Score += 1
+
+        if (self.gameBoard[0][3] == 1 and self.gameBoard[1][2] == 1 and
+               self.gameBoard[2][1] == 1 and self.gameBoard[3][0] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][4] == 1 and self.gameBoard[1][3] == 1 and
+               self.gameBoard[2][2] == 1 and self.gameBoard[3][1] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][3] == 1 and self.gameBoard[2][2] == 1 and
+               self.gameBoard[3][1] == 1 and self.gameBoard[4][0] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][5] == 1 and self.gameBoard[1][4] == 1 and
+               self.gameBoard[2][3] == 1 and self.gameBoard[3][2] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][4] == 1 and self.gameBoard[2][3] == 1 and
+               self.gameBoard[3][2] == 1 and self.gameBoard[4][1] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][3] == 1 and self.gameBoard[3][2] == 1 and
+               self.gameBoard[4][1] == 1 and self.gameBoard[5][0] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[0][6] == 1 and self.gameBoard[1][5] == 1 and
+               self.gameBoard[2][4] == 1 and self.gameBoard[3][3] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][5] == 1 and self.gameBoard[2][4] == 1 and
+               self.gameBoard[3][3] == 1 and self.gameBoard[4][2] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][4] == 1 and self.gameBoard[3][3] == 1 and
+               self.gameBoard[4][2] == 1 and self.gameBoard[5][1] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[1][6] == 1 and self.gameBoard[2][5] == 1 and
+               self.gameBoard[3][4] == 1 and self.gameBoard[4][3] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][5] == 1 and self.gameBoard[3][4] == 1 and
+               self.gameBoard[4][3] == 1 and self.gameBoard[5][2] == 1):
+            self.player1Score += 1
+        if (self.gameBoard[2][6] == 1 and self.gameBoard[3][5] == 1 and
+               self.gameBoard[4][4] == 1 and self.gameBoard[5][3] == 1):
+            self.player1Score += 1
+
+        # Check player 2
+        if (self.gameBoard[2][0] == 2 and self.gameBoard[3][1] == 2 and
+               self.gameBoard[4][2] == 2 and self.gameBoard[5][3] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][0] == 2 and self.gameBoard[2][1] == 2 and
+               self.gameBoard[3][2] == 2 and self.gameBoard[4][3] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][1] == 2 and self.gameBoard[3][2] == 2 and
+               self.gameBoard[4][3] == 2 and self.gameBoard[5][4] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][0] == 2 and self.gameBoard[1][1] == 2 and
+               self.gameBoard[2][2] == 2 and self.gameBoard[3][3] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][1] == 2 and self.gameBoard[2][2] == 2 and
+               self.gameBoard[3][3] == 2 and self.gameBoard[4][4] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][2] == 2 and self.gameBoard[3][3] == 2 and
+               self.gameBoard[4][4] == 2 and self.gameBoard[5][5] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][1] == 2 and self.gameBoard[1][2] == 2 and
+               self.gameBoard[2][3] == 2 and self.gameBoard[3][4] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][2] == 2 and self.gameBoard[2][3] == 2 and
+               self.gameBoard[3][4] == 2 and self.gameBoard[4][5] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][3] == 2 and self.gameBoard[3][4] == 2 and
+               self.gameBoard[4][5] == 2 and self.gameBoard[5][6] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][2] == 2 and self.gameBoard[1][3] == 2 and
+               self.gameBoard[2][4] == 2 and self.gameBoard[3][5] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][3] == 2 and self.gameBoard[2][4] == 2 and
+               self.gameBoard[3][5] == 2 and self.gameBoard[4][6] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][3] == 2 and self.gameBoard[1][4] == 2 and
+               self.gameBoard[2][5] == 2 and self.gameBoard[3][6] == 2):
+            self.player2Score += 1
+
+        if (self.gameBoard[0][3] == 2 and self.gameBoard[1][2] == 2 and
+               self.gameBoard[2][1] == 2 and self.gameBoard[3][0] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][4] == 2 and self.gameBoard[1][3] == 2 and
+               self.gameBoard[2][2] == 2 and self.gameBoard[3][1] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][3] == 2 and self.gameBoard[2][2] == 2 and
+               self.gameBoard[3][1] == 2 and self.gameBoard[4][0] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][5] == 2 and self.gameBoard[1][4] == 2 and
+               self.gameBoard[2][3] == 2 and self.gameBoard[3][2] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][4] == 2 and self.gameBoard[2][3] == 2 and
+               self.gameBoard[3][2] == 2 and self.gameBoard[4][1] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][3] == 2 and self.gameBoard[3][2] == 2 and
+               self.gameBoard[4][1] == 2 and self.gameBoard[5][0] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[0][6] == 2 and self.gameBoard[1][5] == 2 and
+               self.gameBoard[2][4] == 2 and self.gameBoard[3][3] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][5] == 2 and self.gameBoard[2][4] == 2 and
+               self.gameBoard[3][3] == 2 and self.gameBoard[4][2] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][4] == 2 and self.gameBoard[3][3] == 2 and
+               self.gameBoard[4][2] == 2 and self.gameBoard[5][1] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[1][6] == 2 and self.gameBoard[2][5] == 2 and
+               self.gameBoard[3][4] == 2 and self.gameBoard[4][3] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][5] == 2 and self.gameBoard[3][4] == 2 and
+               self.gameBoard[4][3] == 2 and self.gameBoard[5][2] == 2):
+            self.player2Score += 1
+        if (self.gameBoard[2][6] == 2 and self.gameBoard[3][5] == 2 and
+               self.gameBoard[4][4] == 2 and self.gameBoard[5][3] == 2):
+            self.player2Score += 1
+
